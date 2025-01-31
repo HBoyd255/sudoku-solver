df37fae7ccd9d77de525c8bd8cb7c5a9ebbf9214
Harry Boyd <hboyd255@gmail.com>
2025-01-31 17:25:18 +0000

    Input sample sudoku

--- src/main.c
+++ src/main.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main() {
+// https://en.wikipedia.org/wiki/sudoku
+char *inputSudoku =
+    "53##7####"
+    "6##195###"
+    "#98####6#"
+    "8###6###3"
+    "4##8#3##1"
+    "7##2###6"
+    "#6####28#"
+    "###419##5"
+    "####8##79";
 
-    printf("Hello World");
-
-}
+void main() { printf(inputSudoku); }
