a8087226ecde25f5534a554f5d9348fbb3daa065
Harry Boyd <hboyd255@gmail.com>
2025-02-01 01:16:13 +0000

    added function to number cells

--- src/main.c
+++ src/main.c
@@ -3,22 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-// https://en.wikipedia.org/wiki/sudoku
-char *inputSudoku =
-    "53##7####"
-    "6##195###"
-    "#98####6#"
-    "8###6###3"
-    "4##8#3##1"
-    "7###2###6"
-    "#6####28#"
-    "###419##5"
-    "####8##79";
-
 void printSudoku(char *sudoku) {
     char cell;
 
-    for (uint8_t i = 1; i <= 81; i++) {
+    for (uint8_t i = 1; i < 82; i++) {
         cell = sudoku[i - 1];
 
         if (cell == '#') {
@@ -38,4 +26,49 @@ void printSudoku(char *sudoku) {
     }
 }
 
-void main() { printGrid(inputSudoku); }
+void numberSudokuCells(char *sudoku) {
+    // Row number
+    printf("Rows\n");
+    for (uint8_t i = 0; i < 81; i++) {
+        printf("%2d", i / 9);
+        if (!((i + 1) % 9)) {
+            printf("\n");
+        }
+    }
+
+    // Column number
+    printf("\nColumns\n");
+    for (uint8_t i = 0; i < 81; i++) {
+        printf("%2d", i % 9);
+        if (!((i + 1) % 9)) {
+            printf("\n");
+        }
+    }
+
+    // Grid number
+    printf("\nGrids\n");
+    for (uint8_t i = 0; i < 81; i++) {
+        // printf("%2d", (i / 3) % 3);
+        printf("%2d", ((i / 3) % 3) + ((i / 27) * 3));
+
+        if (!((i + 1) % 9)) {
+            printf("\n");
+        }
+    }
+}
+
+void main() {
+    // https://en.wikipedia.org/wiki/sudoku
+    static char *inputSudoku =
+        "53##7####"
+        "6##195###"
+        "#98####6#"
+        "8###6###3"
+        "4##8#3##1"
+        "7###2###6"
+        "#6####28#"
+        "###419##5"
+        "####8##79";
+
+    numberSudokuCells(inputSudoku);
+}
