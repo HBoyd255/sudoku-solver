93603a6297655c40e63e92137d79862b7320b65b
Harry Boyd <hboyd255@gmail.com>
2025-02-03 20:13:39 +0000

    added nonClueCells_ptrarr to sudoku struct

--- src/main.c
+++ src/main.c
@@ -3,46 +3,80 @@
 #include <stdlib.h>
 #include <string.h>
 
-uint16_t checkMasks[10] = {0, 1, 2, 4, 8, 16, 32, 64, 128, 256};
-
-#define ROW_NUMBER(i) i / 9
-#define COLUMN_NUMBER(i) i % 9
-#define GRID_NUMBER(i) ((i / 3) % 3) + ((i / 27) * 3)
-
-uint16_t rowChecks[9] = {0};
-uint16_t columnChecks[9] = {0};
-uint16_t gridChecks[9] = {0};
-
+/**
+ * @brief A struct to represent a cell within a Sudoku. This stores the value of
+ * the cell, whether the value was provided as a clue (and therefor should not
+ * be edited), as well as 3 pointers to check codes, used to calculate the
+ * presence of other numbers within the cell's row, column and group.
+ */
 typedef struct {
     uint8_t value;
     uint8_t is_clue;
 
-    uint16_t *rowChecker;
-    uint16_t *columnChecker;
-    uint16_t *gridChecker;
-
+    uint16_t *rowCheck_ptr;
+    uint16_t *columnCheck_ptr;
+    uint16_t *gridCheck_ptr;
 } cell_t;
 
-#define CELL_INIT                \
-    (cell_t){.value = 0,         \
-             .is_clue = 0,       \
-             .rowChecker = 0,    \
-             .columnChecker = 0, \
-             .gridChecker = 0}
-
+/**
+ * @brief A strucy representing the Sudoku.
+ *
+ * The Sudoku contains an array of 81 cells, as well as  27 check codes. 9 for
+ * each row, column, and group.
+ *
+ * A check code is a 9 bit unsigned integer value, where each bit represents
+ * whether each number is present or not.
+ *
+ * For example, a row with the check code "0b001001101" would contain a 1,
+ * 3,4 and 7. The locates of each number are not stored by each check code.
+ */
 typedef struct {
+    // An array of all 81 cells making up the Sudoku.
     cell_t cells[81];
+
+    // An array of pointers, to cells that need to be solved.This allows for
+    // better traversal while solving the Sudoku, without the need to skip over
+    // the clues.
+    cell_t *nonClueCells_ptrarr[81];
+
+    // The number of cells that need solving.
+    uint8_t nonClueCount;
+
+    // Arrays of pointers to all 27 check codes.
+    uint16_t rowCheck_arr[9];
+    uint16_t columnCheck_arr[9];
+    uint16_t gridCheck_arr[9];
 } sudoku_t;
 
-void printCheckCode(uint16_t data) {
-    for (uint8_t i = 0; i < 9; i++) {
-        printf("%c", ((data & 1)) ? '1' + i : '.');
+// Gets the row number of a cell(both 0-indexed)
+#define ROW_NUMBER(i) i / 9
 
-        data = data >> 1;
+// Gets the column number of a cell(both 0-indexed)
+#define COLUMN_NUMBER(i) i % 9
+
+// Gets the grid number of a cell(both 0-indexed)
+#define GRID_NUMBER(i) ((i / 3) % 3) + ((i / 27) * 3)
+
+// An array of masks for check codes.
+static const uint16_t checkMasks[10] = {0, 1, 2, 4, 8, 16, 32, 64, 128, 256};
+
+/**
+ * @brief Prints the value of a check code to the terminal, by replacing each
+ * bit with the corrsponding value.
+ *
+ * For example the check code "0b001001101" would be printed as "1.34..7.."
+ *
+ * @param checkCodeValue The value of the check code to print.
+ */
+static void printCheckCode(uint16_t checkCodeValue) {
+    for (uint8_t i = 0; i < 9; i++) {
+        printf("%c", ((checkCodeValue & 1)) ? '1' + i : '.');
+
+        checkCodeValue = checkCodeValue >> 1;
     }
 }
 
-void printSudokuString(char *sudokuString) {
+static void printSudokuString(char *sudokuString) {
     char cell;
 
     for (uint8_t i = 0; i < 82; i++) {
@@ -66,7 +100,9 @@ void printSudokuString(char *sudokuString) {
         }
     }
 }
-void printSudoku(sudoku_t *sudoku) {
+
+static void printSudoku(sudoku_t *sudoku) {
+    printf("%d cells to solve.\n", sudoku->nonClueCount);
     for (uint8_t i = 0; i < 81; i++) {
         printf("%d", sudoku->cells[i].value);
 
@@ -82,47 +118,76 @@ void printSudoku(sudoku_t *sudoku) {
     printf("\n");
     printf("Row         Column      Grid\n");
     for (uint8_t i = 0; i < 9; i++) {
-        // printf("[%3d] [%3d] [%3d]\n", rowChecks[i], columnChecks[i],
-        //        gridChecks[i]);
-
         printf("[");
-        printCheckCode(rowChecks[i]);
+        printCheckCode(sudoku->rowCheck_arr[i]);
         printf("] [");
-        printCheckCode(columnChecks[i]);
+        printCheckCode(sudoku->columnCheck_arr[i]);
         printf("] [");
-        printCheckCode(gridChecks[i]);
+        printCheckCode(sudoku->gridCheck_arr[i]);
         printf("]");
         printf("\n");
     }
 }
 
-void initialiseSudoku(sudoku_t *sudoku, char *sudokuString) {
-    char cellChar;
+static void printSudokuSimple(sudoku_t *sudoku) {
+    for (uint8_t i = 0; i < 81; i++) {
+        if (sudoku->cells[i].value) {
+            printf("%d", sudoku->cells[i].value);
+        } else {
+            printf(".");
+        }
+
+        if (!((i + 1) % 9)) {
+            printf("\n");
+        } else if (!((i + 1) % 3)) {
+            printf("|");
+        }
+        if ((i + 1) == 27 || (i + 1) == 54) {
+            printf("---+---+---\n");
+        }
+    }
+}
+
+static void initialiseSudoku(sudoku_t *sudoku, char *sudokuString) {
+    // Set all the check codes to 0.
+    for (uint8_t i = 0; i < 9; i++) {
+        sudoku->rowCheck_arr[i] = 0;
+        sudoku->columnCheck_arr[i] = 0;
+        sudoku->gridCheck_arr[i] = 0;
+    }
+
+    sudoku->nonClueCount = 0;
 
     for (uint8_t i = 0; i < 81; i++) {
-        cellChar = sudokuString[i];
         cell_t *cell = &sudoku->cells[i];
 
-        *cell = CELL_INIT;
+        uint8_t numerical = sudokuString[i] - '0';
 
-        uint8_t numerical = cellChar - '0';
-
-        cell->rowChecker = &rowChecks[ROW_NUMBER(i)];
-        cell->columnChecker = &columnChecks[COLUMN_NUMBER(i)];
-        cell->gridChecker = &gridChecks[GRID_NUMBER(i)];
+        cell->rowCheck_ptr = &sudoku->rowCheck_arr[ROW_NUMBER(i)];
+        cell->columnCheck_ptr = &sudoku->columnCheck_arr[COLUMN_NUMBER(i)];
+        cell->gridCheck_ptr = &sudoku->gridCheck_arr[GRID_NUMBER(i)];
 
         if (numerical <= 9) {
             cell->value = numerical;
             cell->is_clue = 1;
 
-            *cell->rowChecker |= checkMasks[numerical];
-            *cell->columnChecker |= checkMasks[numerical];
-            *cell->gridChecker |= checkMasks[numerical];
+            *cell->rowCheck_ptr |= checkMasks[numerical];
+            *cell->columnCheck_ptr |= checkMasks[numerical];
+            *cell->gridCheck_ptr |= checkMasks[numerical];
+        } else {
+            cell->value = 0;
+            cell->is_clue = 0;
+
+            sudoku->nonClueCells_ptrarr[sudoku->nonClueCount] = cell;
+
+            sudoku->nonClueCount++;
         }
     }
 }
 
 int main() {
+    system("cls");
+
     // https://en.wikipedia.org/wiki/sudoku
     static char *inputSudokuString =
         "53##7####"
@@ -135,13 +200,15 @@ int main() {
         "###419##5"
         "####8##79";
 
-    printSudokuString(inputSudokuString);
+    // printSudokuString(inputSudokuString);
 
     static sudoku_t sudoku;
 
     initialiseSudoku(&sudoku, inputSudokuString);
 
-    printf("\n\n");
+    // printf("\n\n");
 
-    printSudoku(&sudoku);
+    // printSudoku(&sudoku);
+
+    printSudokuSimple(&sudoku);
 }
